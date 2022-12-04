31c12b3e69d8afc7ddee2ebfd77093e4cf45db8a
George Tian <github@crowdlabel.org>
2022-12-04 16:17:59 +0800

    update

--- code/simple-router.cpp
+++ code/simple-router.cpp
@@ -37,7 +37,7 @@ SimpleRouter::handlePacket(const Buffer& packet, const std::string& inIface)
 
     std::cerr << getRoutingTable() << std::endl;
 
-    print_hdr_eth(packet->data);
+    print_hdr_eth(packet.data);
 
     const ethernet_hdr *ehdr = (const ethernet_hdr *)packet.data;
 
